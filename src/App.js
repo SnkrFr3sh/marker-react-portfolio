9fe526a310a66c68a1144338dae08a405297f976
Marc Pacampara <mpacampara@gmail.com>
2022-03-31 16:57:06 -0700

    added arrow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import {BrowserRouter, Link, Routes, Route} from 'react-router-dom'
-
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.js'
 import './App.css';
+
+
+
 
 function App() {
   return (
     <section>
-    <BrowserRouter>
-      <div>
-          <div class='landingContainer'>
+      <BrowserRouter>
+        <div class='scrollPrompt'>
+          <p class='scrollText arrowText' to="/" href="/"
+          >^</p>
+          {/* <p class='scrollText'> SCROLL UP</p> */}
+        </div>
+        <div>
+          <div class='landingContainer circle-leave-active'>
             <div class='landingText'>
               MARC PACAMPARA
             </div>
           </div>
-      </div>
+        </div>
 
-      {/* <Routes>
+
+        <Routes>
         <Route path='/' element={<Home />} />
-      </Routes> */}
-    </BrowserRouter>
-  </section>
+      </Routes>
+      </BrowserRouter>
+    </section>
   );
 }
 
