aa7551a2f5b1c96f17703fccde3be5c8ffe4bdbc
Marc Pacampara <mpacampara@gmail.com>
2022-07-15 12:41:06 -0700

    working on work page section

--- src/pages/Work/work.js
+++ src/pages/Work/work.js
@@ -3,10 +3,31 @@ import './work.css'
 
 export default function Work() {
     return (
-        <div class="workBody">
+        <section class="workBody">
+            <div class='workTitle'>
+                <div class='workTitleText'>PROJECTS</div>
+                <div className='workTitleBox'></div>
+            </div>
+            <div class='workSection'>
+                {/* <div class='row'> */}
+                    <div class='project1'>
+                        <div class='workSectionBorder'>
+                            <div class='workSection'>
+                                <div class='workText'>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='project2'>
+                        <div class='techworkSection'>
+                            <div class='techworkText'>
+                            </div>
+                        </div>
+                    </div>
+                {/* </div> */}
+            </div>
             <div class='workTitle'>Work</div>
-        </div>
-
+        </section>
 
     );
 }
