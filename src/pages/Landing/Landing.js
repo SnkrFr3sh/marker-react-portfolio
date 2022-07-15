74d6cf413a32ba5777044ac84802bf3b61b9eb4c
Marc Pacampara <mpacampara@gmail.com>
2022-07-15 12:21:47 -0700

    polished about section

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -46,72 +46,88 @@ export default function Landing() {
         </div> */}
       </section>
       <section class='homeBody'>
-        <section class='homeSection'>
+        {/* <section class='homeSection'> */}
           <div class='aboutTitle'>
-            <div className='aboutTitleBox'></div>
             <div class='aboutTitleText'>ABOUT</div>
-          </div>
-          <div class='homeBox'></div>
-          <div>
+            <div className='aboutTitleBox'></div>
+
+
+            {/* <div class='homeBox'></div> */}
+            {/* <div>
             <img src={homePhoto} alt='' class='homePhoto'></img>
-          </div>
-          {/* <div>
+          </div> */}
+
+                    </div>
+            <div class='aboutColumnContainer container'>
+              <div class='row'>
+                <div class='aboutLeft col-md-6  mx-auto'>
+                <div class='aboutSectionBorder'>
+                  <div>
+                    <img src={homePhoto} alt='' class='homePhoto'></img>
+                  </div>
+                  {/* <div>
             <img src={pixelPhoto} alt='' class='pixelPhoto'></img>
           </div> */}
-          <div class='aboutSectionBorder'></div>
-          <div class='aboutSection'>
-            <div class='aboutText'>
-              <p>I am a full-stack web developer based in Seattle. I bring 20+ years of human based artistry and design experience to digital needs. Lorem ipsum dolor sit amet consectetur adipisicing elit. Auos?
-              </p>
-            </div>
-          </div>
+                  
+                  <div class='aboutSection'>
+                    <div class='aboutText'>
+                      <p>I am a full-stack web developer based in Seattle. I bring 20+ years of human based artistry and design experience to digital needs. Lorem ipsum dolor sit amet consectetur adipisicing elit. Auos?
+                      </p>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+                {/* <div class='techAboutSectionBorder'></div> */}
+                <div class='aboutRight col-md-6  mx-auto'>
+                  <div class='techAboutSection'>
+                    <div class='techAboutText'>
+                      {/* <div class='iconContainer'> */}
+                      <div class='iconTile'>
+                        <img src={htmlIcon} alt='' class='icon icon1' />
+                        <p class='iconText'>HTML</p>
+                      </div>
+                      <div class='iconTile'>
+                        <img src={cssIcon} alt='' class='icon icon2' />
+                        <p class='iconText'>CSS</p>
+                      </div>
+                      <div class='iconTile'>
+                        <img src={jsIcon} alt='' class='icon icon3' />
+                        <p class='iconText'>Javascript</p>
+                      </div>
+                      <div class='iconTile'>
+                        <img src={reactjsIcon} alt='' class='icon icon4' />
+                        <p class='iconText'>React.js</p>
+                      </div>
+                      <div class='iconTile'>
+                        <img src={mongoIcon} alt='' class='icon icon5' />
+                        <p class='iconText'>MongoDB</p>
+                      </div>
+                      <div class='iconTile'>
+                        <img src={sqlIcon} alt='' class='icon icon6' />
+                        <p class='iconText'>MySQL</p>
+                      </div>
+                      <div class='iconTile'>
+                        <img src={expressIcon} alt='' class='icon icon6' />
+                        <p class='iconText'>Express.js</p>
+                      </div>
+                      <div class='iconTile'>
+                        <img src={nodeIcon} alt='' class='icon icon8' />
+                        <p class='iconText'>Node.js</p>
+                      </div>
+                      <div class='iconTile'>
+                        <img src={sassIcon} alt='' class='icon icon9' />
+                        <p class='iconText'>SASS</p>
+                      </div>
 
-          {/* <div class='techAboutSectionBorder'></div> */}
-          <div class='techAboutSection'>
-            <div class='techAboutText'>
-              {/* <div class='iconContainer'> */}
-                <div class='iconTile'>
-                  <img src={htmlIcon} alt='' class='icon icon1' />
-                  <p class='iconText'>HTML</p>
+                      {/* </div> */}
+                      {/* I am an full-stack web developer based in Seattle. I bring 20+ years of human based artistry and design experience to digital needs. */}
+                    </div>
+                  </div>
                 </div>
-                 <div class='iconTile'>
-                  <img src={cssIcon} alt='' class='icon icon2' />
-                  <p class='iconText'>CSS</p>
-                </div>
-                 <div class='iconTile'>
-                  <img src={jsIcon} alt='' class='icon icon3' />
-                  <p class='iconText'>Javascript</p>
-                </div>
-                 <div class='iconTile'>
-                  <img src={reactjsIcon} alt='' class='icon icon4' />
-                  <p class='iconText'>React.js</p>
-                </div>
-                 <div class='iconTile'>
-                  <img src={mongoIcon} alt='' class='icon icon5' />
-                  <p class='iconText'>MongoDB</p>
-                </div>
-                 <div class='iconTile'>
-                  <img src={sqlIcon} alt='' class='icon icon6' />
-                  <p class='iconText'>MySQL</p>
-                </div>
-                 <div class='iconTile'>
-                  <img src={expressIcon} alt='' class='icon icon6' />
-                  <p class='iconText'>Express.js</p>
-                </div>
-                 <div class='iconTile'>
-                  <img src={nodeIcon} alt='' class='icon icon8' />
-                  <p class='iconText'>Node.js</p>
-                </div>
-                 <div class='iconTile'>
-                  <img src={sassIcon} alt='' class='icon icon9' />
-                  <p class='iconText'>SASS</p>
-                </div>
-
-              {/* </div> */}
-              {/* I am an full-stack web developer based in Seattle. I bring 20+ years of human based artistry and design experience to digital needs. */}
+              </div>
             </div>
-          </div>
-        </section>
+
+        {/* </section> */}
       </section>
 
     </div>
