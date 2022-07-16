623280296d00587de22bde02910d6e151ad82c98
Marc Pacampara <mpacampara@gmail.com>
2022-07-15 23:12:38 -0700

    finished left project card section

--- src/pages/Work/work.js
+++ src/pages/Work/work.js
@@ -10,32 +10,46 @@ export default function Work() {
             </div>
             <div class='workSection'>
                 {/* <div class='row'> */}
-                    <div class='project1'>
-                            <div class='project1Section'>
-                                <div class='project1Text'>
-                                    <div class='projectTitle'>
-                                    <p>Tailor Thrift</p>
-                                    <p>E-Commerce Website</p>
-                                    </div>
-
-                                    <p class='projectDesc'>Full Stack E-commerce website built with REACT, Node.js, Express.js, MongoDB, </p>
-                                    <div class='project1Live'>
-                                    <div class='project1LiveBtn'>LIVE SITE</div>
-                                    <div class='project1LiveBtnLine'></div>
-                                    </div>
-                                    <div class='project1LearnMore'>
-                                    <div class='project1LearnMoreBtn'>LEARN MORE</div>
-                                    <div class='project1LearnMoreBtnLine'></div>
-                                    </div>
-                                </div>
+                <div class='project1'>
+                    <div class='project1Section'>
+                        <div class='project1Text'>
+                            <div class='projectTitle'>
+                                <p>Tailor Thrift</p>
+                                <p>E-Commerce Website</p>
                             </div>
-                    </div>
-                    <div class='project2'>
-                        <div class='techworkSection'>
-                            <div class='techworkText'>
+
+                            <p class='projectDesc'>Full Stack E-commerce website built with REACT, Node.js, Express.js, MongoDB, </p>
+                            <div class='project1Live'>
+                                <div class='project1LiveBtn'>LIVE SITE</div>
+                                <div class='project1LiveBtnLine'></div>
+                            </div>
+                            <div class='project1LearnMore'>
+                                <div class='project1LearnMoreBtn'>LEARN MORE</div>
+                                <div class='project1LearnMoreBtnLine'></div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class='project1'>
+                    <div class='project1Section'>
+                        <div class='project1Text'>
+                            <div class='project1Title'>
+                                <p>Tailor Thrift</p>
+                                <p>E-Commerce Website</p>
+                            </div>
+
+                            <p class='projectDesc'>Full Stack E-commerce website built with REACT, Node.js, Express.js, MongoDB, </p>
+                            <div class='project1Live'>
+                                <div class='project1LiveBtn'>LIVE SITE</div>
+                                <div class='project1LiveBtnLine'></div>
+                            </div>
+                            <div class='project1LearnMore'>
+                                <div class='project1LearnMoreBtn'>LEARN MORE</div>
+                                <div class='project1LearnMoreBtnLine'></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 {/* </div> */}
             </div>
             <div class='workTitle'>Work</div>
