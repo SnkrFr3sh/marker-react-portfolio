5888bbb6a9d6db2b346bbfe1ae541125793765af
Marc Pacampara <mpacampara@gmail.com>
2022-07-15 23:12:19 -0700

    finished left project card section

--- src/pages/Work/work.js
+++ src/pages/Work/work.js
@@ -11,12 +11,24 @@ export default function Work() {
             <div class='workSection'>
                 {/* <div class='row'> */}
                     <div class='project1'>
-                        <div class='workSectionBorder'>
-                            <div class='workSection'>
-                                <div class='workText'>
+                            <div class='project1Section'>
+                                <div class='project1Text'>
+                                    <div class='projectTitle'>
+                                    <p>Tailor Thrift</p>
+                                    <p>E-Commerce Website</p>
+                                    </div>
+
+                                    <p class='projectDesc'>Full Stack E-commerce website built with REACT, Node.js, Express.js, MongoDB, </p>
+                                    <div class='project1Live'>
+                                    <div class='project1LiveBtn'>LIVE SITE</div>
+                                    <div class='project1LiveBtnLine'></div>
+                                    </div>
+                                    <div class='project1LearnMore'>
+                                    <div class='project1LearnMoreBtn'>LEARN MORE</div>
+                                    <div class='project1LearnMoreBtnLine'></div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                     </div>
                     <div class='project2'>
                         <div class='techworkSection'>
