077b13b4490a3cbe7fc194ddac454fe8c3ca86e7
Marc Pacampara <mpacampara@gmail.com>
2022-07-18 22:05:35 -0700

    testing browerrouter locally

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-// import BrowserRouter from 'react-router-dom/BrowserRouter'
-import { Switch, Routes, Route } from 'react-router-dom';
-import { HashRouter } from "react-router-dom";
-
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom"
 import './App.css';
-// import Home from './pages/Home/Home';
 import Landing from './pages/Landing/Landing'
-// import Work from './pages/Work/work'
 
 
 function App() {
   
   return (
     <section class='body'>
-      <div>test</div>
-        <HashRouter>
-        {/* <BrowserRouter> */}
-          <Switch>
-            <Route path='/' exact element={<Landing  />} />
+        <BrowserRouter>
+
+          <Routes>
+            <Route path='/' element={<Landing  />} />
             {/* <Route path='/home' element={<Home />} />
             <Route path='/Work' element={<Work />} /> */}
-          </Switch>
-        {/* </BrowserRouter> */}
-        </HashRouter>
+          </Routes>
+        </BrowserRouter>
     </section>
 
   );
