25c1d590ba5f0f2e13372675bbce89d31dc652d9
Marc Pacampara <mpacampara@gmail.com>
2022-07-25 22:53:52 -0700

    testing test div

--- src/App.js
+++ src/App.js
@@ -1,53 +1,20 @@
-// import { BrowserRouter } from 'react-router-dom'
-// import { Routes, Route } from "react-router-dom"
-// import './App.css';
-// import Landing from './pages/Landing/Landing'
-
-
-// function App() {
-  
-//   return (
-//     <section class='body'>
-//         <BrowserRouter>
-
-//           <Routes>
-//             <Route path='/' element={<Landing  />} />
-//             {/* <Route path='/home' element={<Home />} />
-//             <Route path='/Work' element={<Work />} /> */}
-//           </Routes>
-//         </BrowserRouter>
-//     </section>
-
-//   );
-// }
-
-// export default App;
 import React from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home/Home';
 import Landing from './pages/Landing/Landing'
-import Work from './pages/Work/work'
 
 
 function App() {
   
   return (
     <section class='body'>
-
-        <BrowserRouter>
+      <div class='test'> this is a test</div>
+        {/* <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Landing />} />
-            {/* <Route path='/'  basename={process.env.PUBLIC_URL} component={<Landing />} /> */}
-
-            {/* <Route path='/home' element={<Home />} /> */}
-            {/* {/* <Route path='/Projects' element={<Projects />} /> */}
-            {/* <Route path='/Work' element={<Work />} /> */}
-            {/* <Route path='/Aboutme' element={<Aboutme />} /> */} 
+            <Route exact path='/' element={<Landing />} />
 
           </Routes>
-        </BrowserRouter>
+        </BrowserRouter> */}
     </section>
       );
 }
